c23ba5d9f26d2cda396aa3386de63b6fda3361e3
McKendree Strommer <mckendree.strommer@gmail.com>
2021-10-09 16:10:53 -0700

    added GameScreen to style GameGrid a bit and make it easier to track

---
+++ src/components/GameScreen/GameScreen.js
@@ -0,0 +1,9 @@
+import GameGrid from "../GameGrid/GameGrid";
+
+const GameScreen = () => (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <GameGrid />
+    </div>
+);
+
+export default GameScreen;
