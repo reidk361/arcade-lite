311d6657a98df4e8d445fe38e6a018775153d8d7
McKendree Strommer <mckendree.strommer@gmail.com>
2021-10-20 18:21:59 -0700

    added basic apollo client

--- src/components/EndGame/EndGame.js
+++ src/components/EndGame/EndGame.js
@@ -1,11 +1,24 @@
 import Score from '../Score/Score';
+import { useQuery } from '@apollo/client';
+import { GET_SCORES } from '../services/scoresGQL';
+
 const EndGame = () => {
+  const { loading, error, data } = useQuery(GET_SCORES);
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
       <h1 style={{ padding: '10px', margin: '10px' }}>
         TOP TEN TETRIS SCORES:
-        <h2>(TOP TEN)</h2>
-        <div >
+        {loading && <p>loading...</p>}
+        {error && <p>{error}</p>}
+        {data &&
+          data.notes.map(({ username, score, _id }) => (
+            <div key={_id}>
+              <p>
+                {username}: {score}
+              </p>
+            </div>
+          ))}
+        <div>
           YOUR SCORE:
           <Score />
         </div>
---
+++ src/services/scoresGQL.js
@@ -0,0 +1,19 @@
+import {
+    ApolloClient,
+    InMemoryCache,
+    gql,
+  } from '@apollo/client';
+  
+  export const client = new ApolloClient({
+    uri: 'http://localhost:4000/',
+    cache: new InMemoryCache(),
+  });
+  
+  export const GET_SCORES = gql`
+    query scores {
+      scores {
+        username
+        score
+      }
+    }
+  `;
