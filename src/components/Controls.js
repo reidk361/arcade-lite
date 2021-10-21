231e1f0400db873ba14604eee514e531efab0d30
Ryan Logan <ryan_plogan@yahoo.com>
2021-10-20 20:28:24 -0700

    fixed button functionality

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,24 +1,33 @@
 import { GridContext } from './GameGrid/GridContextProvider';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+
 
 export default function Controls(props) {
+  const [start, setStart]= useState(true)
   //TODO move this where apropriate and name
   const { startPieceMove, movePiece, pieceName } =
     useContext(GridContext);
 
   function handleControls(e) {
     if (e.target.attributes.id.textContent === 'START' || e.keyCode === 76) {
+      if(start) {
+      setStart(false)
       startPieceMove();
+      }
     } else if (
       e.target.attributes.id.textContent === 'left-button' ||
       e.keyCode === 37
     ) {
-      movePiece({ x: -1, y: 0 }, pieceName);
+      if(start === false) {
+        movePiece({ x: -1, y: 0 }, pieceName);
+      }
     } else if (
       e.target.attributes.id.textContent === 'right-button' ||
       e.keyCode === 39
     ) {
-      movePiece({ x: +1, y: 0 }, pieceName);
+      if(start === false) {
+        movePiece({ x: +1, y: 0 }, pieceName);
+      }
     } else if (
       e.target.attributes.id.textContent === 'rotate-button' ||
       e.keyCode === 32
@@ -28,7 +37,9 @@ export default function Controls(props) {
       e.target.attributes.id.textContent === 'down-button' ||
       e.keyCode === 40
     ) {
-      movePiece({ x: 0, y: +1 }, pieceName, true);
+      if(start === false) {
+        movePiece({ x: 0, y: +1 }, pieceName);
+      }
     }
   }
 
